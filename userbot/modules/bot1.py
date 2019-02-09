44a76842213ae5bc32eefe7b1f425d06dd18e431
JEEPEO <47288442+Jeepeo@users.noreply.github.com>
2019-02-09 11:17:26 +0530

    Update bot1.py

--- userbot/modules/bot1.py
+++ userbot/modules/bot1.py
@@ -6,6 +6,6 @@ from telethon import events
 @bot.on(events.MessageEdited(outgoing=True, pattern="^.bot$"))
 async def amialive(e):
     if not e.text[0].isalpha() and e.text[0] not in ("/", "#", "@", "!"):
-        await e.edit("` \n  ╲╲╭━━━━╮ \n ╭╮┃▆┈┈▆┃╭╮ \n ┃╰┫▽▽▽┣╯┃ \n ╰━┫△△△┣━╯ \n ╲╲┃┈┈┈┈┃  \n ╲╲┃┈┏┓┈┃ `")
+        await e.edit("` \n   ╲╲╭━━━━╮ \n ╭╮┃▆┈┈▆┃╭╮ \n ┃╰┫▽▽▽┣╯┃ \n ╰━┫△△△┣━╯ \n ╲╲┃┈┈┈┈┃  \n ╲╲┃┈┏┓┈┃ `")
                      
              
