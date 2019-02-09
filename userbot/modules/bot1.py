90fff1c4033f19e5d98dd38678ba00a8630d0b84
JEEPEO <47288442+Jeepeo@users.noreply.github.com>
2019-02-09 11:10:04 +0530

    Add files via upload

---
+++ userbot/modules/bot1.py
@@ -0,0 +1,11 @@
+from userbot import bot
+from telethon import events
+
+
+@bot.on(events.NewMessage(outgoing=True, pattern="^.bot$"))
+@bot.on(events.MessageEdited(outgoing=True, pattern="^.bot$"))
+async def amialive(e):
+    if not e.text[0].isalpha() and e.text[0] not in ("/", "#", "@", "!"):
+        await e.edit("` \n ╲╲╭━━━━╮ \n  ╭╮┃▆┈┈▆┃╭╮ \n  ┃╰┫▽▽▽┣╯┃ \n  ╰━┫△△△┣━╯ \n   ╲╲┃┈┈┈┈┃  \n  ╲╲┃┈┏┓┈┃ `")
+                     
+             
