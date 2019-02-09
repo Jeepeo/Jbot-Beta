c6010f32bd122693c5c72493b9698bc34cd790df
JEEPEO <47288442+Jeepeo@users.noreply.github.com>
2019-02-09 20:40:54 +0530

    Update bot1.py

--- userbot/modules/bot1.py
+++ userbot/modules/bot1.py
@@ -15,7 +15,7 @@ async def amialive(e):
     if not e.text[0].isalpha() and e.text[0] not in ("/", "#", "@", "!"):
         await e.edit("`\n╭╭━━━╮╮┈┈┈┈┈┈┈┈┈┈\n┈┃╭━━╯┈┈┈┈▕╲▂▂╱▏┈\n┈┃┃╱▔▔▔▔▔▔▔▏╱▋▋╮┈\n┈┃╰▏┃╱╭╮┃╱╱▏╱╱▆┃┈\n┈╰━▏┗━╰╯┗━╱╱╱╰┻┫┈\n┈┈┈▏┏┳━━━━▏┏┳━━╯┈\n┈┈┈▏┃┃┈┈┈┈▏┃┃┈┈┈┈ `")
 
-@bot.on(events.MessageEdited(outgoing=True, pattern="^.hey$"))
+@bot.on(events.NewMessage(outgoing=True, pattern="^.hey$"))
 @bot.on(events.MessageEdited(outgoing=True, pattern="^.hey$"))
 async def amialive(e):
     if not e.text[0].isalpha() and e.text[0] not in ("/", "#", "@", "!"):
