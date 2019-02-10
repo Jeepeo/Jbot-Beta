7f5d7bc685d15f12cec60b4f1f9d4b84d16ea664
jeepeo <mohammedsabeeh3@gmail.com>
2019-02-10 13:45:06 +0530

    Added_Build_Number_in_botver

--- userbot/modules/misc.py
+++ userbot/modules/misc.py
@@ -333,7 +333,7 @@ async def sysdetails(sysd):
 async def bot_ver(e):
     if not e.text[0].isalpha() and e.text[0] not in ("/", "#", "@", "!"):
         ver = (
-            "`UserBot Version: modular 2.2-a (Jeepeo-Edition😎) "
+                "`UserBot Version: modular 2.2-a (Jeepeo-Edition😎) (build-09022019)"
             + subprocess.run(
                 [
                     "git",
