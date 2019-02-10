9402d2ae7c446e62ec36b80e2c048996e3a61470
jeepeo <mohammedsabeeh3@gmail.com>
2019-02-10 14:45:39 +0530

    add

--- userbot/modules/purge.py
+++ userbot/modules/purge.py
@@ -66,6 +66,7 @@ async def purgeme(delme):
                 LOGGER_GROUP, "Purge of " + str(count) + " messages done successfully."
             )
         time.sleep(2)
+        i = 1
         await smsg.delete()
 
 
