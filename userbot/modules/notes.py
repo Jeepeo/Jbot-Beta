8227bf32e4a9c6b37ca365942c89081ed0b9f6f1
baalajimaestro <baalajimaestro@computer4u.com>
2019-02-24 13:26:38 +0530

    userbot: notes: Use register as the wrapper
    Signed-off-by: baalajimaestro <baalajimaestro@computer4u.com>

--- userbot/modules/notes.py
+++ userbot/modules/notes.py
@@ -22,7 +22,7 @@ async def notes_active(svd):
         await svd.edit(message)
 
 
-@bot.on(events.NewMessage(outgoing=True, pattern="^\.clear (\w*)"))
+@register(outgoing=True, pattern="^\.clear (\w*)")
 async def remove_notes(clr):
     if not clr.text[0].isalpha() and clr.text[0] not in ("/", "#", "@", "!"):
         try:
@@ -35,7 +35,7 @@ async def remove_notes(clr):
         await clr.edit("```Note removed successfully```")
 
 
-@bot.on(events.NewMessage(outgoing=True, pattern="^\.save (\w*)"))
+@register(outgoing=True, pattern="^\.save (\w*)")
 async def add_filter(fltr):
     if not fltr.text[0].isalpha():
         try:
@@ -54,7 +54,7 @@ async def add_filter(fltr):
         )
 
 
-@bot.on(events.NewMessage(pattern="#\w*"))
+@register(pattern="#\w*")
 async def incom_note(getnt):
     try:
         if not (await getnt.get_sender()).bot:
