3487c70a5b6817a93148d01dfa7cffbc47ea73e2
jeepeo <mohammedsabeeh3@gmail.com>
2019-03-05 14:02:49 +0530

    [ADD]Added StickerPackInfo modules , COPYTRIGHTED to @nitanmarcel

--- userbot/modules/TG-companion.py
+++ userbot/modules/TG-companion.py
@@ -5,7 +5,13 @@ import aiohttp
 import bs4
 from userbot.events import register
 from telethon import events
-
+from userbot import bot
+from telethon.tl.types import (DocumentAttributeFilename,
+                               InputMediaUploadedDocument,
+                               InputStickerSetShortName,
+                               MessageMediaPhoto,
+                               InputStickerSetID)
+from telethon.tl.types import DocumentAttributeSticker
 
 @register(outgoing=True, pattern="^.gstfy$")
 async def gangstafy_text(event):
@@ -47,3 +53,29 @@ def __remove_emojis(text):
         if char in list(emoji.EMOJI_UNICODE.values()):
             clean_text = re.sub(char, "", clean_text)
     return clean_text
+
+@register(outgoing=True, pattern="^.packinfo$")
+async def get_pack_info(event):
+    if not event.is_reply:
+        await bot.update_message(event, PACKINFO_HELP)
+        return
+    rep_msg = await event.get_reply_message()
+    if not rep_msg.document:
+        await bot.update_message(event, "`Reply to a sticker to get the pack details`")
+        return
+    stickerset_attr = rep_msg.document.attributes[1]
+    if not isinstance(stickerset_attr, DocumentAttributeSticker):
+        await bot.update_message(event, "`Not a valid sticker`")
+        return
+    get_stickerset = await bot(GetStickerSetRequest(InputStickerSetID(id=stickerset_attr.stickerset.id, access_hash=stickerset_attr.stickerset.access_hash)))
+    pack_emojis = []
+    for document_sticker in get_stickerset.packs:
+        if document_sticker.emoticon not in pack_emojis:
+            pack_emojis.append(document_sticker.emoticon)
+    OUTPUT = f"**Sticker Title:** `{get_stickerset.set.title}\n`" \
+             f"**Sticker Short Name:** `{get_stickerset.set.short_name}`\n" \
+             f"**Official:** `{get_stickerset.set.official}`\n" \
+             f"**Archived:** `{get_stickerset.set.archived}`\n" \
+             f"**Stickers In Pack:** `{len(get_stickerset.packs)}`\n" \
+             f"**Emojis In Pack:** {' '.join(pack_emojis)}"
+    await event.edit(OUTPUT)
